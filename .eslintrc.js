04edba944003a13635ad7f6a8a8ee75d8052a935
n-devs <n.devs260340@gmail.com>
2024-08-31 23:54:19 +0700

    update

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaVersion: 6,
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };
+  
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 80,
+    semi: true,
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+  };
+  
